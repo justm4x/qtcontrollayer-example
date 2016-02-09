82c4e03014e4352c2a1f6d5e4f1abdd9a4e5604a
max <mlevdik@gmail.com>
2016-02-09 23:36:07 +0200

    add qtcontrollayer submodule. simple test for Controller and QIntProperty

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,10 +1,21 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
+
+#include "qtcontrollayer/Controller.h"
+#include "qtcontrollayer/QIntProperty.h"
 
 int main(int argc, char * argv[])
 {
     QGuiApplication application(argc, argv);
     QQmlApplicationEngine engine;
     engine.load(QCoreApplication::applicationDirPath() + "/qml/main.qml");
+
+    Controller * controller = new Controller("Example");
+    QIntProperty * p = controller->registerProperty<QIntProperty>("CurrentIndex", QObject::tr("Current Index"), 3, 0, 10);
+    qDebug() << p->displayName() << ": " << p->displayValue();
+    p->setValue(5);
+    qDebug() << p->displayName() << ": " << p->displayValue();
+
     return application.exec();
 }
